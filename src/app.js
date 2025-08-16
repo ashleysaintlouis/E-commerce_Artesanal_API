b4d15db0eebf164b0efa6e4b6201f79ca6c9ce43
saintlouisashlhey998 <saintlouisashlhey998@gmail.com>
2025-08-16 14:15:13 +0000

    Import initDatabase

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+import { initDatabase } from './database';
+initDatabase();
 
 const authRoutes = require('./routes/authRoutes');
 const statusRoutes = require('./routes/statusRoutes'); // Importa a nova rota
